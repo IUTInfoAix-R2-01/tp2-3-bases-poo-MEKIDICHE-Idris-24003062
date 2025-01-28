31c865c782b74b04de22b35b8c4bb92cdb43c4fb
Mekidiche <MEKIDICHE-Idris-24003062>
2025-01-28 15:48:46 +0100

    ajout du code cercle

---
+++ src/cercle.java
@@ -0,0 +1,39 @@
+public class cercle {
+	private double radius;
+	private String color;
+	
+	public Circle() {
+		radius = 1.0;
+		color = "red";
+	}
+	
+	public Circle(double r) {
+		radius = r;
+		color = "red";
+	}
+	
+	public Circle(double r, String c) {
+		radius = r;
+		color = c;
+	}
+	
+	public String getColor() {
+		return color;
+	}
+	
+	public double getRadius() {
+		return radius;
+	}
+	
+	public double getArea() {
+		return radius*radius*Math.PI;
+	}
+	
+	public void setRadius(double newRadius) {
+		radius = newRadius;
+	}
+	
+	public void setColor(String newColor) {
+		color = newColor;
+	}
+}
